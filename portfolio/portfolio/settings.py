156ab2aea713a8e99dd2fe21419251e285486367
MohammadSalkhorde <m.salkhorde444@gmail.com>
2025-10-11 19:48:05 +0330

    update hero_section.html

--- portfolio/portfolio/settings.py
+++ portfolio/portfolio/settings.py
@@ -26,9 +26,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-7uq0m)s7cjcj7swve*v9ontau)0=#hul8v#1ilgaj2_l0j5*gg'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = []
 
 
 # Application definition
